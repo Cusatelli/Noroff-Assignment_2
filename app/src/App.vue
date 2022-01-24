d14810c4636315a462688ad5313c13f280ca1343
Cusatelli <einarvandevelde@gmail.com>
2022-01-24 08:56:20 +0100

    test?(branch): rename to Start to test branches.

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Start from './components/Start.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Start msg="Trivia App - Start" />
 </template>
 
 <style>
--- app/src/components/Start.vue
+++ app/src/components/Start.vue
@@ -29,7 +29,7 @@ const count = ref(0)
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
     Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
+    <code>components/Start.vue</code> to test hot module replacement.
   </p>
 </template>
 
