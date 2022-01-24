59b54c7e129f4bd5218acf57f266f0b4f8aa0c64
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-01-24 19:23:37 +0100

    fix:added option template

--- app/src/router.js
+++ app/src/router.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import Start from './views/Start.vue';
 import AuthGuard from './views/AuthGuard.vue';
+import Options from './views/Options.vue'
+import Game from './views/Game.vue'
 
 const routes = [
     {
@@ -12,6 +14,14 @@ const routes = [
         path: "/login", // For users
         component: AuthGuard // For devs
     },
+    {
+        path: "/options", // Options
+        component: Options // Options
+    },
+    {
+        path: "/game", // game
+        component: Game // Game
+    }
     // Example:
     // {
     //     path: "/profile",
--- app/src/views/options.vue
+++ app/src/views/options.vue
@@ -1,9 +1,55 @@
 <script setup>
 
+function onPlay() {
+    console.log("Go to game");
+    router.push('/Game');
+}
 </script>
 
 <template>
+<div class="option-container">
     <h1>options</h1>
+        <form>
+    
+    <fieldset>
+        <label for="difficulty">Difficulty</label>
+            <select v-model="selected">
+                <option disabled value="">Please select one</option>
+                <option>Easy</option>
+                <option>Medium</option>
+                <option>Hard</option>
+            </select>
+            <span>Selected: {{ selected }}</span>
+    </fieldset>
+
+    <fieldset>
+        <label for="category">Category</label>
+            <select v-model="selected">
+                <option disabled value="">Please select one</option>
+                <option>FRuits</option>
+                <option>Vegetable</option>
+                <option>Cats</option>
+            </select>
+            <span>Selected: {{ selected }}</span>
+    </fieldset>
+
+    <fieldset>
+        <label for="numberOfQuestions">Number of questions</label>
+            <select v-model="selected">
+                <option disabled value="">Please select one</option>
+                <option>5</option>
+                <option>10</option>
+                <option>15</option>
+            </select>
+            <span>Selected: {{ selected }}</span>
+    </fieldset>
+
+    <div>
+        <button type="button" class="start-button" @click="onPlay">Play</button>
+    </div>
+    
+</form>
+</div>
 </template>
 
 <style scoped>
