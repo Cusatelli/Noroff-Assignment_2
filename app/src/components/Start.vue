c413038e76701f2f1e4595b180c006f1d91c87aa
Cusatelli <einarvandevelde@gmail.com>
2022-01-24 09:14:07 +0100

    feat: go to 'login' button

--- app/src/components/Start.vue
+++ app/src/components/Start.vue
@@ -1,40 +1,14 @@
 <script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
+function goToLogin() {
+    console.log("Go to login");
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/Start.vue</code> to test hot module replacement.
-  </p>
+    <div class="start-container">
+        <button class="start-button" @click="goToLogin">Start</button>
+    </div>
 </template>
 
 <style scoped>
-a {
-  color: #42b983;
-}
 </style>
