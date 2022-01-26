933c84edb42ab7e77bce19340ce97afa132bfd77
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-01-26 19:52:29 +0100

    docs: JSDoc functions, deleted ; and changed " to '

--- app/src/store.js
+++ app/src/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'vuex'
 import { API_KEY, API_URL } from './api'
 
+/**
+ * 
+ * @returns storedUser in JSON format from localstorage
+ */
 const initUser = () => {
     const storedUser = localStorage.getItem('user')
     if (!storedUser) {
@@ -10,6 +14,9 @@ const initUser = () => {
     return JSON.parse(storedUser)
 }
 
+/**
+ * States including userinfo
+ */
 export default createStore({
     state: {
         user: initUser(),
@@ -18,9 +25,9 @@ export default createStore({
         results: null
     },
     getters: {
-        
+
     },
-    mutations: {
+    mutations: {// Storeoptions that stores userinfo in a mutationTree
         setUser: (state, user) => {
             state.user = user
         },
@@ -34,43 +41,42 @@ export default createStore({
             state.results = results
         }
     },
-    actions: {
+    actions: {// Storeoptions that stores user results in a actionTree
         async gameResults({ commit }, { questions, answers, correctAnswers }) {
             // Compare answers and correct answers.
-            let results = 0;
+            let results = 0
             for (let index = 0; index < correctAnswers.length; index++) {
-                const correctAnswer = correctAnswers[index];
-                const answer = answers[index];
+                const correctAnswer = correctAnswers[index]
+                const answer = answers[index]
                 if (correctAnswer === answer) {
-                    results += 10;
+                    results += 10
                 }
             }
 
-            const gameResults = { questions, answers, correctAnswers, results };
-            commit('setResults', gameResults);
-        },
+            const gameResults = { questions, answers, correctAnswers, results }
+            commit('setResults', gameResults)
+        },// A method for user input options, if statements minimum number of questions 2 and maximum 10
         async inputOptions({ commit }, { difficulty, category, type, numberOfQuestions }) {
-            // Clamp
             if (numberOfQuestions > 10) {
-                numberOfQuestions = 10;
-                console.log(`Oops! You can not have more than ${numberOfQuestions} questions!`);
+                numberOfQuestions = 10
+                console.log(`Oops! You can not have more than ${numberOfQuestions} questions!`)
             } else if (numberOfQuestions < 2) {
-                numberOfQuestions = 2;
-                console.log(`Oops! You can not have less than ${numberOfQuestions} questions!`);
+                numberOfQuestions = 2
+                console.log(`Oops! You can not have less than ${numberOfQuestions} questions!`)
             }
-            if (type === "multiple choice") { type = "multiple"; }
-            else { type = "boolean"; }
+            if (type === 'multiple choice') { type = 'multiple' }
+            else { type = 'boolean' }
 
             const inputs = {
-                difficulty: difficulty || "easy",
-                category: category || (Math.floor(Math.random() * 23) + 9) + "", // Random number between 9 & 32
-                type: type || "multiple",
-                numberOfQuestions: numberOfQuestions || "5"
+                difficulty: difficulty || 'easy',
+                category: category || (Math.floor(Math.random() * 23) + 9) + '', // Random number between 9 & 32
+                type: type || 'multiple',
+                numberOfQuestions: numberOfQuestions || '5'
             }
 
-            commit('setInputs', inputs);
-            localStorage.setItem('options', JSON.stringify(inputs));
-        },
+            commit('setInputs', inputs)
+            localStorage.setItem('options', JSON.stringify(inputs))
+        },// Updateing user id and score
         async updateUserInfo({ commit }, { userID, highScore }) {
             try {
                 await fetch(`${API_URL}/trivia/${userID}`, {
@@ -84,26 +90,26 @@ export default createStore({
                         highScore
                     })
                 })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Could not update high score')
-                    }
-                    return response.json()
-                })
-                .then(updatedUser => {
-                    // updatedUser is the user with the Patched data
-                    commit('setUser', updatedUser);
-                    return null;
-                })
-                .catch(error => {
-                    throw new Error(error);
-                })
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Could not update high score')
+                        }
+                        return response.json()
+                    })
+                    .then(updatedUser => {
+                        // updatedUser is the user with the Patched data
+                        commit('setUser', updatedUser)
+                        return null
+                    })
+                    .catch(error => {
+                        throw new Error(error)
+                    })
 
                 return null
             } catch (error) {
                 return error.message
             }
-        },
+        },//Function for user login and fetching user data from api
         async loginUser({ commit }, { action, username }) {
             try {
                 if (action !== 'login') {
@@ -114,11 +120,11 @@ export default createStore({
                     .then(response => response.json())
                     .then(results => {
                         // results will be an array of users that match the username of mega-mind.
-                        console.log(results);
+                        console.log(results)
                         if (results.length > 0) {
                             // Login as that user
                             commit('setUser', results) // commit = mutations
-                            return null;
+                            return null
                         } else {
                             // Create and login user
                             fetch(`${API_URL}/trivia`, {
@@ -132,23 +138,23 @@ export default createStore({
                                     highScore: 0
                                 })
                             })
-                            .then(response => {
-                                if (!response.ok) {
-                                    throw new Error('Could not create new user')
-                                }
-                                return response.json()
-                            })
-                            .then(newUser => {
-                                commit('setUser', newUser) // commit = mutations
-                                return null;
-                            })
-                            .catch(error => {
-                                throw new Error(error);
-                            })
+                                .then(response => {
+                                    if (!response.ok) {
+                                        throw new Error('Could not create new user')
+                                    }
+                                    return response.json()
+                                })
+                                .then(newUser => {
+                                    commit('setUser', newUser) // commit = mutations
+                                    return null
+                                })
+                                .catch(error => {
+                                    throw new Error(error)
+                                })
                         }
                     })
                     .catch(error => {
-                        throw new Error(error);
+                        throw new Error(error)
                     })
 
                 return null
