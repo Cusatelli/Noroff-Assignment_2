71d35b630570d20f18d5747d28cde58f92eb1357
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-01-26 17:42:16 +0100

    refactor: deleted semicolon and changed "" to ''

--- app/src/views/Game.vue
+++ app/src/views/Game.vue
@@ -1,86 +1,86 @@
 <script>
-import { useStore } from 'vuex';
+import { useStore } from 'vuex'
 import { onMounted, ref } from 'vue'
-import { apiGenerateURL } from '../api/apiURL';
-import { useRouter } from 'vue-router';
+import { apiGenerateURL } from '../api/apiURL'
+import { useRouter } from 'vue-router'
 
 export default {
 	setup() {
-		const store = useStore();
-		const router = useRouter();
-		const questions = ref([]);
-		const alternatives = ref([]);
-		const answers = ref([]);
-		const correctAnswers = ref([]);
-		const question = ref("");
-		let index = 0;
+		const store = useStore()
+		const router = useRouter()
+		const questions = ref([])
+		const alternatives = ref([])
+		const answers = ref([])
+		const correctAnswers = ref([])
+		const question = ref('')
+		let index = 0
 
 		onMounted(async () => {
-			console.log("Mounted")
-			question.value = "Loading..."
-			const options = JSON.parse(localStorage.getItem('options'));
+			console.log('Mounted')
+			question.value = 'Loading...'
+			const options = JSON.parse(localStorage.getItem('options'))
 
 			await fetch(apiGenerateURL(options))
 				.then(response => response.json())
 				.then(results => {
 					// results will be an array of users that match the username of mega-mind.
-					questions.value = results.results;
+					questions.value = results.results
 
-					setQuestion(0);
-				});
+					setQuestion(0)
+				})
 		})
 
 		function setQuestion(index) {
 			if (!questions.value[index].question) {
-				console.error("Oops! Something went wrong, please try again.");
-				router.push(options);
+				console.error('Oops! Something went wrong, please try again.')
+				router.push(options)
 			}
-			question.value = questions.value[index].question;
+			question.value = questions.value[index].question
 			alternatives.value = questions.value[index].incorrect_answers
-			alternatives.value.push(questions.value[index].correct_answer);
-			correctAnswers.value.push(questions.value[index].correct_answer);
+			alternatives.value.push(questions.value[index].correct_answer)
+			correctAnswers.value.push(questions.value[index].correct_answer)
 		}
 
 		function nextQuestion(answer) {
-			console.log("next")
+			console.log('next')
 			if (answer) { // Always add answer
-				answers.value.push(answer);
-				console.log(answers.value);
+				answers.value.push(answer)
+				console.log(answers.value)
 			} else { // if i.e skip is pressed
-				answers.value.push(null);
-				console.log(answers.value);
+				answers.value.push(null)
+				console.log(answers.value)
 			}
 
-			const nextQuestion = questions.value[++index];
+			const nextQuestion = questions.value[++index]
 			if (nextQuestion) {
-				setQuestion(index);
-				console.log(correctAnswers.value);
+				setQuestion(index)
+				console.log(correctAnswers.value)
 			} else {
 				// Compare answers and correct answers.
-				// let results = 0;
-				// for (let index = 0; index < correctAnswers.value.length; index++) {
-				// 	const correctAnswer = correctAnswers.value[index];
-				// 	const answer = answers.value[index];
+				// let results = 0
+				// for (let index = 0 index < correctAnswers.value.length index++) {
+				// 	const correctAnswer = correctAnswers.value[index]
+				// 	const answer = answers.value[index]
 				// 	if (correctAnswer === answer) {
-				// 		results += 10;
+				// 		results += 10
 				// 	}
 				// }
 
 				// Go to results screen
-				// router.push('/results');
+				// router.push('/results')
 				let questionsQuestion = []
 				for (let index = 0; index < questions.value.length; index++) {
-					questionsQuestion.push(questions.value[index].question);
+					questionsQuestion.push(questions.value[index].question)
 				}
-				store.dispatch("gameResults", {
+				store.dispatch('gameResults', {
 					questions: questionsQuestion,
 					answers: answers.value,
 					correctAnswers: correctAnswers.value
 				}).then(() => {
-					router.push('/results');
+					router.push('/results')
 				})
-				// console.log("Results " + results + " Points");
-				return question.value;
+				// console.log('Results ' + results + ' Points')
+				return question.value
 			}
 		}
 
