26004ef2636af24b35536fdfbf4ed662996f2dfa
Cusatelli <einarvandevelde@gmail.com>
2022-01-26 11:41:35 +0100

    fix: calculate results in result & dispatch instead of push.

--- app/src/views/Game.vue
+++ app/src/views/Game.vue
@@ -31,12 +31,11 @@ export default {
 		})
 
 		function setQuestion(index) {
-			question.value = questions.value[index].question;
-			if(!question.value) {
+			if (!questions.value[index].question) {
 				console.error("Oops! Something went wrong, please try again.");
 				router.push(options);
 			}
-
+			question.value = questions.value[index].question;
 			alternatives.value = questions.value[index].incorrect_answers
 			alternatives.value.push(questions.value[index].correct_answer);
 			correctAnswers.value.push(questions.value[index].correct_answer);
@@ -44,36 +43,43 @@ export default {
 
 		function nextQuestion(answer) {
 			console.log("next")
-			if(answer) { // Always add answer
+			if (answer) { // Always add answer
 				answers.value.push(answer);
 				console.log(answers.value);
 			} else { // if i.e skip is pressed
 				answers.value.push(null);
 				console.log(answers.value);
 			}
-			
+
 			const nextQuestion = questions.value[++index];
-			if(nextQuestion) {
+			if (nextQuestion) {
 				setQuestion(index);
 				console.log(correctAnswers.value);
 			} else {
 				// Compare answers and correct answers.
-				let results = 0;
-				for (let index = 0; index < correctAnswers.value.length; index++) {
-					const correctAnswer = correctAnswers.value[index];
-					const answer = answers.value[index];
-					if(correctAnswer === answer) {
-						results += 10;
-					}
-				}
-				
+				// let results = 0;
+				// for (let index = 0; index < correctAnswers.value.length; index++) {
+				// 	const correctAnswer = correctAnswers.value[index];
+				// 	const answer = answers.value[index];
+				// 	if (correctAnswer === answer) {
+				// 		results += 10;
+				// 	}
+				// }
+
 				// Go to results screen
-				router.push('/results');
-				// store.dispatch("resultsToResults", { answers: answers.value, correctAnswers: correctAnswers.value, results })
-				// 	.then(() => {
-				// 		router.push('/results');
-				// 	})
-				console.log("Results " + results + " Points")
+				// router.push('/results');
+				let questionsQuestion = []
+				for (let index = 0; index < questions.value.length; index++) {
+					questionsQuestion.push(questions.value[index].question);
+				}
+				store.dispatch("gameResults", {
+					questions: questionsQuestion,
+					answers: answers.value,
+					correctAnswers: correctAnswers.value
+				}).then(() => {
+					router.push('/results');
+				})
+				// console.log("Results " + results + " Points");
 				return question.value;
 			}
 		}
@@ -91,11 +97,13 @@ export default {
 	<div class="container game">
 		<h2>{{ question }}</h2>
 		<ul class="questions">
-			<li	v-for="(answer, index) in alternatives"
+			<li
+				v-for="(answer, index) in alternatives"
 				:key="index"
-				:value="answer" class="button" @click="nextQuestion(answer)">
-				{{ answer }}
-			</li>
+				:value="answer"
+				class="button"
+				@click="nextQuestion(answer)"
+			>{{ answer }}</li>
 		</ul>
 		<button class="next" @click="nextQuestion(null)">Skip</button>
 	</div>
