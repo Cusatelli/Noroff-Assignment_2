eecd3aba35cd5a4cf1a1aa8ef8fe30c0e203b840
Cusatelli <einarvandevelde@gmail.com>
2022-01-26 10:24:45 +0100

    feat: basic results page (no data sent)

---
+++ app/src/views/Results.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function restart() {
+    router.push('/game');
+}
+
+function exit() {
+    router.push('/');
+}
+</script>
+
+<template>
+    <div class="container results">
+        <img alt="Trivia Logo" src="../assets/logo.png" class="logo" />
+        <div class="buttons">
+            <button class="restart" @click="restart">Restart</button>
+            <button class="exit" @click="exit">Exit</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
